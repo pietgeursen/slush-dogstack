5254a916357dc479881d6df239ff260d1362caab
Piet Geursen <pietgeursen@gmail.com>
2017-02-20 11:55:37 +1300

    Does not write files to dir.

--- slushfile.js
+++ slushfile.js
@@ -1,13 +1,58 @@
 var gulp = require('gulp')
 var install = require('gulp-install')
 var conflict = require('gulp-conflict')
-var template = require('gulp-template')
+//var template = require('gulp-template')
+var {template} = require('lodash')
 var rename = require('gulp-rename')
 var inquirer = require('inquirer')
 var licenses = require('osi-licenses')
 
+var pull = require('pull-stream')
+var onEnd = require('pull-stream/sinks/on-end')
+var fromPromise = require('pull-promise')
+var task = require('pull-task')
+var vinyl = require('pull-vinyl')
+
 gulp.task('default', function (done) {
-  inquirer.prompt([{
+  pull(
+    fromPromise.source(prompt()),
+    pull.filter(answers => answers.moveon),
+    pull.asyncMap((answers, cb) => {
+      pull(
+        // Note use of __dirname to be relative to generator
+        vinyl.src(__dirname + '/template/**/*.*', {
+          // Include dotfiles
+          dot: true
+        }),
+        // Rename dotfiles
+        pull.map(file => {
+          if (file.basename[0] === '_') {
+            file.basename = '.' + file.basename.slice(1)
+          }
+          return file
+        }),
+        pull.map(file => {
+          if(!file.isNull()){
+            var tpl = template(file.contents.toString());
+            file.contents = new Buffer(tpl(Object.assign({}, file.data, answers)));
+          }
+          return file
+        }),
+        vinyl.dest(null, cb)
+      )
+      // Confirms overwrites on file conflicts
+      //  .pipe(conflict('./'))
+      // Without __dirname here = relative to cwd
+      //  .pipe(gulp.dest('./'))
+      // Run `bower install` and/or `npm install` if necessary
+      //  .pipe(install())
+    }),
+    onEnd(done)
+  )
+})
+
+function prompt() {
+  return inquirer.prompt([{
     type: 'input',
     name: 'name',
     message: 'Give your app a name',
@@ -34,31 +79,4 @@ gulp.task('default', function (done) {
     name: 'moveon',
     message: 'Continue?'
   }])
-    .then(function (answers) {
-      if (!answers.moveon) {
-        return done()
-      }
-      // Note use of __dirname to be relative to generator
-      gulp.src(__dirname + '/template/**', {
-        // Include dotfiles
-        dot: true
-      })
-        // Lodash template support
-        .pipe(template(answers))
-        // Rename dotfiles
-        .pipe(rename(function (file) {
-          if (file.basename[0] === '_') {
-            file.basename = '.' + file.basename.slice(1)
-          }
-        }))
-        // Confirms overwrites on file conflicts
-        .pipe(conflict('./'))
-        // Without __dirname here = relative to cwd
-        .pipe(gulp.dest('./'))
-        // Run `bower install` and/or `npm install` if necessary
-        .pipe(install())
-        .on('finish', function () {
-          done() // Finished!
-        })
-    })
-})
+}
