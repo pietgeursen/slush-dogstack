f950e83b6a89802efcc3c1d57415da8366f63f21
Piet Geursen <pietgeursen@gmail.com>
2017-02-24 09:05:08 +1300

    Remove old templates

--- template/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/app'
-
-render(<App name='<%= name %>' />, document.querySelector('main'))
-console.log('welcome to <%= name %>')
--- template/test/index.js
+++
@@ -1,6 +0,0 @@
-var test = require('tape')
-
-test('testing <%=name %>', function (t) {
-  t.ok(true)
-  t.end()
-})
